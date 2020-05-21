b3b386ddaa7eb1884a0dee4d4eed0208934713e5
misun9283 <kimmisun9283@naver.com>
2020-05-21 22:05:33 +0900

    [add]댓글 기능 추가 - 재로그인후 이전 댓글 보여주기 구현

--- account/utils.py
+++ account/utils.py
@@ -1,16 +1,20 @@
 import json, jwt, requests
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from insta_ex.settings import SECRET_KEY
 from account.models  import Account
 
 def login_decorator(func):
 	def wrapper(self, request, *args, **kwargs):
+		if access_token:
 		try:
 			access_token    = request.headers.get('Authorization',None)
 			payload         = jwt.decode(access_token, SECRET_KEY, algorithm='HS256')
 			user            = Account.objects.get(name = payload['name'])
 			request.user    = user
+		
+		except KeyError:
+			return HttpResponse(status = 400)
 
 		except jwt.exceptions.DecodeError:
 			return JsonResponse({ 'message' : 'INVALID_TOKEN' }, status = 400)
--- comment/views.py
+++ comment/views.py
@@ -30,14 +30,13 @@ class CommentView(View):
 
 	@login_decorator
 	def get(self,request):
-		comment_data = list(Comment.objects.values())
-		values_list =[]
-		for comment in comment_data:
-			values_dic				= {}
-			values_dic['name']		= comment.account.name
-			values_dic['content']	= comment.account.content
-			values_list.append(values_dic)
-
-		return JsonResponse({ 'data':values_list },status =200)
+		
+		comment_data = [{
+                 'id'        : comment.id,
+				 'name'      : comment.account.name,
+				 'content'   : comment.content,
+				 'created_at': comment.created_at
+             } for comment in Comment.objects.all()]
+		return JsonResponse({ 'data':comment_data },status =200)
 
 
